e4ae3aab3bb1082fc8a4bc5c736db7adb851ed8e
kangyh <kangyh@gmail.com>
2018-02-13 04:39:20 +0900

    graph paint

---
+++ Chap2/mook_GraphPaint/mook_GraphPaint/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace mook_GraphPaint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnGraph = new System.Windows.Forms.Button();
+            this.btnBar = new System.Windows.Forms.Button();
+            this.GraphCore = new mook_GraphCore.mook_GraphCore();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnGraph
+            // 
+            this.btnGraph.Location = new System.Drawing.Point(407, 256);
+            this.btnGraph.Name = "btnGraph";
+            this.btnGraph.Size = new System.Drawing.Size(75, 23);
+            this.btnGraph.TabIndex = 0;
+            this.btnGraph.Text = "Graph";
+            this.btnGraph.UseVisualStyleBackColor = true;
+            this.btnGraph.Click += new System.EventHandler(this.btnGraph_Click);
+            // 
+            // btnBar
+            // 
+            this.btnBar.Location = new System.Drawing.Point(488, 256);
+            this.btnBar.Name = "btnBar";
+            this.btnBar.Size = new System.Drawing.Size(75, 23);
+            this.btnBar.TabIndex = 1;
+            this.btnBar.Text = "Bar";
+            this.btnBar.UseVisualStyleBackColor = true;
+            this.btnBar.Click += new System.EventHandler(this.btnBar_Click);
+            // 
+            // GraphCore
+            // 
+            this.GraphCore.Location = new System.Drawing.Point(12, 12);
+            this.GraphCore.Name = "GraphCore";
+            this.GraphCore.Size = new System.Drawing.Size(551, 238);
+            this.GraphCore.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.Time_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 287);
+            this.Controls.Add(this.GraphCore);
+            this.Controls.Add(this.btnBar);
+            this.Controls.Add(this.btnGraph);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "DrawGraph";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGraph;
+        private System.Windows.Forms.Button btnBar;
+        private mook_GraphCore.mook_GraphCore GraphCore;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Chap2/mook_GraphPaint/mook_GraphPaint/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mook_GraphPaint
+{
+    public partial class Form1 : Form
+    {
+        Random r = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GraphCore.OpenType = mook_GraphCore.ChartControlOpenType.Bar;
+        }
+
+        private void Time_Tick(object sender, EventArgs e)
+        {
+            double ValueAdd;
+            int n = r.Next(1, 45);
+            int s = r.Next(1, 3);
+            try
+            {
+                if (s / 2 == 0)
+                    ValueAdd = Convert.ToDouble(n);
+                else
+                    ValueAdd = Convert.ToDouble(-n);
+                GraphCore.AddValue((float)ValueAdd);
+                GraphCore.RefreshControl();
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private void btnGraph_Click(object sender, EventArgs e)
+        {
+            GraphCore.OpenType = mook_GraphCore.ChartControlOpenType.Graph;
+        }
+
+        private void btnBar_Click(object sender, EventArgs e)
+        {
+            GraphCore.OpenType = mook_GraphCore.ChartControlOpenType.Bar;
+        }
+    }
+}
